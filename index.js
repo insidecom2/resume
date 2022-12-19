d60727ccb40f9222ac30a0d83e1e9d7295a61f2e
Jakkrit <>
2022-12-19 09:39:55 +0700

    validate express notic

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const notic = require("./service/sendnotic");
 require('dotenv').config()
 var express = require('express');
 var bodyParser = require('body-parser')
+const { body, validationResult } = require('express-validator');
 
 var app = express();
 // parse application/x-www-form-urlencoded
@@ -17,19 +18,29 @@ app.use(express.static(__dirname + '/public'));
 // index page
 app.get('/', function (req, res) {
   const endPoint = process.env.END_POINT;
-  res.render('pages/index', {endPoint: endPoint});
+  res.render('pages/index', { endPoint: endPoint });
 });
 
 // about page
-app.post('/send-notic', async function (req, res) {
-  try {
-    const response = await notic.sendNotic(req.body)
-    res.json(response)
-    
-  } catch (error) {
-    res.json({ status: false})
-  }
-});
+app.post('/send-notic',
+  body('email').isEmail(),
+  body('name').isLength({ min: 1 }),
+  body('subject').isLength({ min: 1 }),
+  body('message').isLength({ min: 1 }),
+  async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const response = await notic.sendNotic(req.body)
+      res.json(response)
+
+    } catch (error) {
+      res.json({ status: false })
+    }
+  });
 
 app.listen(3000);
 console.log('Server is listening on port http://localhost:3000');
